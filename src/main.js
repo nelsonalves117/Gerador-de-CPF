810e49f0da792d5ba29e10e7ac8fe8b24cdfa6f9
Nelson Alves <nelsonalves117@gmail.com>
2024-01-31 17:44:56 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import GeraCPF from './modules/geraCPF';
+
+import './assets/css/style.css';
+
+(function () {
+    const gera = new GeraCPF();
+    const cpfGerado = document.querySelector('.cpf-gerado');
+    cpfGerado.innerHTML = gera.geraNovoCPF();
+})();
